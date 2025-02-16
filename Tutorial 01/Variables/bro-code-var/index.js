f90ce33bf49f3f658ad5952e74b470bdeba9aa77
Kelvi Yawo Jules Agbessi Awuklu <115982245+NetGhostX@users.noreply.github.com>
2025-02-16 18:47:35 +0100

    BroCode: taking user input, type conversion, constants

--- Tutorial 01/Variables/bro-code-var/index.js
+++ Tutorial 01/Variables/bro-code-var/index.js
@@ -1,3 +1,5 @@
+  
+
 let fullName = "Kelvi Yawo"; //string
 let age = 23; // int
 let student = true; //boolean
@@ -6,3 +8,27 @@ let student = true; //boolean
 document.getElementById("p1").textContent =`Your name is : ${fullName}`;
 document.getElementById("p2").textContent = `You are ${age} years old `;
 document.getElementById("p3").textContent =`Enrolled: ${student}`;
+
+
+//Taker user input. 
+//let username = window.prompt("What's your username ?");
+
+//User input the right way.
+let username2;
+document.getElementById("mysubmit").onclick= function(event){
+       event.preventDefault(); //prevent form submission
+       username2 = document.getElementById("myText").value;
+
+       document.getElementById("myH1").textContent = `Hello ${username2}`
+}
+
+
+//Type Conversion
+
+let age1 = window.prompt("How old are you ?");
+age1 = Number(age);
+
+age1 +=1;
+
+console.log(age1, typeof age1)
+
