f72d694e9b9517c16fe8b4b5bdf1aafaf4600a9e
Kelvi Yawo Jules Agbessi Awuklu <115982245+NetGhostX@users.noreply.github.com>
2025-02-20 18:50:45 +0100

    Callback

---
+++ Callback/index.js
@@ -0,0 +1,14 @@
+//callback = a function that is passed as an argument to another function
+
+hello(goodbye);
+
+function hello(callback){
+    console.log("Hello!");
+}
+
+function leave(){
+    console.log("Leave!!")
+}
+function goodbye(){
+    console.log("Hello!");
+}
